813b2aa8891bdab95deb340f12ae5725b0ebac75
Mani Teja <maniteja.kurapati@gmail.com>
2018-08-19 21:32:27 -0500

    iterative traversals

--- epi_judge_java/epi/TreeInorder.java
+++ epi_judge_java/epi/TreeInorder.java
@@ -1,20 +1,44 @@
 package epi;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.List;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
-import java.util.List;
+
 public class TreeInorder {
-  @EpiTest(testDataFile = "tree_inorder.tsv")
+    @EpiTest(testDataFile = "tree_inorder.tsv")
 
-  public static List<Integer> inorderTraversal(BinaryTreeNode<Integer> tree) {
-    // TODO - you fill in here.
-    return null;
-  }
+    public static List<Integer> inorderTraversal(BinaryTreeNode<Integer> tree) {
 
-  public static void main(String[] args) {
-    System.exit(
-        GenericTest
-            .runFromAnnotations(args, "TreeInorder.java",
-                                new Object() {}.getClass().getEnclosingClass())
-            .ordinal());
-  }
+	Deque<BinaryTreeNode<Integer>> stack = new ArrayDeque<>();
+
+	List<Integer> traversal = new ArrayList<>();
+
+	BinaryTreeNode<Integer> current = tree;
+
+	while (current != null || !stack.isEmpty()) {
+	    if (current != null) {
+		stack.addFirst(current);
+
+		current = current.left;
+	    } else {
+		current = stack.removeFirst();
+
+		traversal.add(current.data);
+
+		current = current.right;
+	    }
+
+	}
+
+	return traversal;
+    }
+
+    public static void main(String[] args) {
+	System.exit(GenericTest.runFromAnnotations(args, "TreeInorder.java", new Object() {
+	}.getClass().getEnclosingClass()).ordinal());
+    }
 }
--- epi_judge_java/epi/TreePreorder.java
+++ epi_judge_java/epi/TreePreorder.java
@@ -1,20 +1,42 @@
 package epi;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.List;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
-import java.util.List;
+
 public class TreePreorder {
-  @EpiTest(testDataFile = "tree_preorder.tsv")
+    @EpiTest(testDataFile = "tree_preorder.tsv")
 
-  public static List<Integer> preorderTraversal(BinaryTreeNode<Integer> tree) {
-    // TODO - you fill in here.
-    return null;
-  }
+    public static List<Integer> preorderTraversal(BinaryTreeNode<Integer> tree) {
+	Deque<BinaryTreeNode<Integer>> stack = new ArrayDeque<>();
 
-  public static void main(String[] args) {
-    System.exit(
-        GenericTest
-            .runFromAnnotations(args, "TreePreorder.java",
-                                new Object() {}.getClass().getEnclosingClass())
-            .ordinal());
-  }
+	List<Integer> traversal = new ArrayList<>();
+
+	BinaryTreeNode<Integer> current = tree;
+
+	while (current != null || !stack.isEmpty()) {
+
+	    if (current != null) {
+		traversal.add(current.data);
+
+		stack.addFirst(current);
+
+		current = current.left;
+	    } else {
+		current = stack.removeFirst();
+
+		current = current.right;
+	    }
+	}
+	return traversal;
+    }
+
+    public static void main(String[] args) {
+	System.exit(GenericTest.runFromAnnotations(args, "TreePreorder.java", new Object() {
+	}.getClass().getEnclosingClass()).ordinal());
+    }
 }
